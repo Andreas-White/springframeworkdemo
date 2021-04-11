821176f0adb11f559e0090b52f5b8762b28535d7
Andreas Manos <antmanos42@gmail.com>
2021-04-11 03:12:05 +0200

    initialising a spring framework demo project

---
+++ src/main/java/com/example/jbrainspringdemo/JbrainspringdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jbrainspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbrainspringdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JbrainspringdemoApplication.class, args);
+    }
+
+}
