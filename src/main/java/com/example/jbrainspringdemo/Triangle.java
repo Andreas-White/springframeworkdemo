d4eacddd7a8e2d5ea49874de1be2cb533e614281
Andreas Manos <antmanos42@gmail.com>
2021-04-11 19:17:02 +0200

    constructor injection

--- src/main/java/com/example/jbrainspringdemo/Triangle.java
+++ src/main/java/com/example/jbrainspringdemo/Triangle.java
@@ -3,16 +3,30 @@ package com.example.jbrainspringdemo;
 public class Triangle implements Shape {
 
     private String type;
+    private int height;
+
+    public Triangle (String type) {
+        this.type = type;
+    }
+
+    public Triangle(String type, int height) {
+        this.type = type;
+        this.height = height;
+    }
 
     public String getType() {
         return type;
     }
 
-    public void setType(String type) {
-        this.type = type;
+    public int getHeight() {
+        return height;
     }
 
+    /*public void setType(String type) {
+        this.type = type;
+    }*/
+
     public void draw() {
-        System.out.println(getType() +" Draw triangle");
+        System.out.println("Draw triangle of type " + getType() + " and height: " + getHeight());
     }
 }
