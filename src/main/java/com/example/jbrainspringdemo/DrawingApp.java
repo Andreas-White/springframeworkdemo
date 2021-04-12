59e32934d026449d4a87ccad8fef7ac4cb43ab03
Andreas Manos <antmanos42@gmail.com>
2021-04-12 03:09:07 +0200

    object injection with inner beans

--- src/main/java/com/example/jbrainspringdemo/DrawingApp.java
+++ src/main/java/com/example/jbrainspringdemo/DrawingApp.java
@@ -9,16 +9,29 @@ public class DrawingApp {
         //BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
 
+        System.out.println("************************");
+        System.out.println("--Constructor injection--");
         // Constructor injection
         Shape triangle = (Triangle) context.getBean("triangle");
         triangle.draw();
+
+        System.out.println("************************");
+        System.out.println("--Setter injection--");
         // Setter injection
         Shape circle = (Circle) context.getBean("circle");
         circle.draw();
 
+        System.out.println("************************");
+        System.out.println("--Object injection--");
         // Object injection
         Shape polygon = (Polygon) context.getBean("polygon");
         polygon.draw();
+
+        System.out.println("************************");
+        System.out.println("--Object injection with inner beans--");
+        // Object injection with inner beans
+        Square square = (Square) context.getBean("square");
+        square.draw();
 
     }
 }
