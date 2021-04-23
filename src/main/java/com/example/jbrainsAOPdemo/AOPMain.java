ec7cf6e3a698c835de264c2573caa43be91c49d2
Andreas Manos <antmanos42@gmail.com>
2021-04-23 04:18:11 +0200

    First run of AOPMain

--- src/main/java/com/example/jbrainsAOPdemo/AOPMain.java
+++ src/main/java/com/example/jbrainsAOPdemo/AOPMain.java
@@ -1,5 +1,6 @@
 package com.example.jbrainsAOPdemo;
 
+import com.example.jbrainsAOPdemo.service.AnimalService;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -8,6 +9,10 @@ public class AOPMain {
     public static void main(String[] args) {
 
         AbstractApplicationContext context = new ClassPathXmlApplicationContext("springAOP.xml");
+        AnimalService service = context.getBean("animalService",AnimalService.class);
 
+        System.out.println(service.getDog().getName());
+        System.out.println(service.getCat().getName());
+        System.out.println(service.getBird().getName());
     }
 }
--- src/main/java/com/example/jbrainsAOPdemo/service/AnimalService.java
+++ src/main/java/com/example/jbrainsAOPdemo/service/AnimalService.java
@@ -1,16 +1,36 @@
 package com.example.jbrainsAOPdemo.service;
 
-import com.example.jbrainsAOPdemo.animals.Animal;
+import com.example.jbrainsAOPdemo.animals.Bird;
+import com.example.jbrainsAOPdemo.animals.Cat;
+import com.example.jbrainsAOPdemo.animals.Dog;
 
 public class AnimalService {
 
-    private Animal animal;
+    private Dog dog;
+    private Cat cat;
+    private Bird bird;
 
-    public Animal getAnimal() {
-        return animal;
+    public Dog getDog() {
+        return dog;
     }
 
-    public void setAnimal(Animal animal) {
-        this.animal = animal;
+    public void setDog(Dog dog) {
+        this.dog = dog;
+    }
+
+    public Cat getCat() {
+        return cat;
+    }
+
+    public void setCat(Cat cat) {
+        this.cat = cat;
+    }
+
+    public Bird getBird() {
+        return bird;
+    }
+
+    public void setBird(Bird bird) {
+        this.bird = bird;
     }
 }
