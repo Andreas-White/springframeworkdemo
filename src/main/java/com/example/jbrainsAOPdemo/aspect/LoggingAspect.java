2e58fee8bb57ba8265041a9b831db8ffd11da190
Andreas Manos <antmanos42@gmail.com>
2021-04-26 02:40:23 +0200

    initial implementation of LoggingAspect class + @Before with wild cards (*)

--- src/main/java/com/example/jbrainsAOPdemo/aspect/LoggingAspect.java
+++ src/main/java/com/example/jbrainsAOPdemo/aspect/LoggingAspect.java
@@ -9,18 +9,25 @@ public class LoggingAspect {
     // Runs this method before the specified method in the Before annotation
     @Before("execution(public String com.example.jbrainsAOPdemo.animals.Dog.getName())")
     public void loggingAdviceDog() {
-        System.out.println("Advice run. Get method called before execution for the Dog class");
+        System.out.println("Advice run. Called before get method execution for the Dog class");
     }
 
     // Runs this method before the specified method in the Before annotation
     @Before("execution(public String com.example.jbrainsAOPdemo.animals.Cat.getName())")
     public void loggingAdviceCat() {
-        System.out.println("Advice run. Get method called before execution for the Cat class");
+        System.out.println("Advice run. Called before get method execution for the Cat class");
     }
 
     // Runs this method before the specified method in the Before annotation
     @Before("execution(public String com.example.jbrainsAOPdemo.animals.Bird.getName())")
     public void loggingAdviceBird() {
-        System.out.println("Advice run. Get method called before execution for the Bird class");
+        System.out.println("Advice run. Called before get method execution for the Bird class");
+    }
+
+    // Is executed before all methods starting with get. The first star (*) implies that is a method
+    // of any type (returning any type or void) and the second (*) any method that starts with "get"
+    @Before("execution(public * get*())")
+    public void loggingAdviceAllGetters() {
+        System.out.println("Advice run. Message displayed before all get methods");
     }
 }
