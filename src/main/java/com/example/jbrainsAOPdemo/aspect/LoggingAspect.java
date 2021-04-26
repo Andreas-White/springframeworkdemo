c2e12a8b661ac6d78cbc398ac2d1010580d67863
Andreas Manos <antmanos42@gmail.com>
2021-04-26 03:02:50 +0200

    initial implementation of LoggingAspect class + @Before with wild cards (*) + @Pointcut

--- src/main/java/com/example/jbrainsAOPdemo/aspect/LoggingAspect.java
+++ src/main/java/com/example/jbrainsAOPdemo/aspect/LoggingAspect.java
@@ -2,12 +2,13 @@ package com.example.jbrainsAOPdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class LoggingAspect {
 
     // Runs this method before the specified method in the Before annotation
-    @Before("execution(public String com.example.jbrainsAOPdemo.animals.Dog.getName())")
+    @Before("execution(public * com.example.jbrainsAOPdemo.animals.Dog.get*())")
     public void loggingAdviceDog() {
         System.out.println("Advice run. Called before get method execution for the Dog class");
     }
@@ -26,8 +27,19 @@ public class LoggingAspect {
 
     // Is executed before all methods starting with get. The first star (*) implies that is a method
     // of any type (returning any type or void) and the second (*) any method that starts with "get"
-    @Before("execution(public * get*())")
+    // Regarding the parameters get() means 0 args, get(..) means 0 or more args and get(*) 1 or more args
+    @Before("execution(public * getName())")
     public void loggingAdviceAllGetters() {
-        System.out.println("Advice run. Message displayed before all get methods");
+        System.out.println("Second Advice run. Message displayed before all get methods");
     }
+
+    @Before("allGetters()")
+    public void loggingAdviceAllPoints() {
+        System.out.println("Third Advice run. Message displayed before all getName methods");
+    }
+
+    // Is if we need to apply the same wild cards for a lot of advices, instead of specifying the wild card
+    // for each advice we can give as argument the name of the method annotated with @Pointcut
+    @Pointcut("execution(public * com.example.jbrainsAOPdemo.animals.Dog.get*())")
+    public void allGetters() {}
 }
