fd0c62983cdafb737528e02bde725260dd2240f3
Andreas Manos <antmanos42@gmail.com>
2021-04-26 02:31:08 +0200

    initial implementation of LoggingAspect class + @Before and spicifying for method in a particular class

--- src/main/java/com/example/jbrainsAOPdemo/aspect/LoggingAspect.java
+++ src/main/java/com/example/jbrainsAOPdemo/aspect/LoggingAspect.java
@@ -6,8 +6,21 @@ import org.aspectj.lang.annotation.Before;
 @Aspect
 public class LoggingAspect {
 
-    @Before("execution(public String getName())")
-    public void loggingAdvice() {
-        System.out.println("Advice run. Get method called before execution");
+    // Runs this method before the specified method in the Before annotation
+    @Before("execution(public String com.example.jbrainsAOPdemo.animals.Dog.getName())")
+    public void loggingAdviceDog() {
+        System.out.println("Advice run. Get method called before execution for the Dog class");
+    }
+
+    // Runs this method before the specified method in the Before annotation
+    @Before("execution(public String com.example.jbrainsAOPdemo.animals.Cat.getName())")
+    public void loggingAdviceCat() {
+        System.out.println("Advice run. Get method called before execution for the Cat class");
+    }
+
+    // Runs this method before the specified method in the Before annotation
+    @Before("execution(public String com.example.jbrainsAOPdemo.animals.Bird.getName())")
+    public void loggingAdviceBird() {
+        System.out.println("Advice run. Get method called before execution for the Bird class");
     }
 }
