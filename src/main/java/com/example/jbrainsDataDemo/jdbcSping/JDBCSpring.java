4d3ffb2d2b6a18aa8ce67fe581234c58cc172a0a
Andreas Manos <antmanos42@gmail.com>
2021-05-02 03:16:23 +0200

    initialising example with Spring

--- src/main/java/com/example/jbrainsDataDemo/jdbcSping/JDBCSpring.java
+++
@@ -1,4 +0,0 @@
-package com.example.jbrainsDataDemo.jdbcSping;
-
-public class JDBCSpring {
-}
---
+++ src/main/java/com/example/jbrainsDataDemo/jdbcSpring/JDBCSpring.java
@@ -0,0 +1,8 @@
+package com.example.jbrainsDataDemo.jdbcSpring;
+
+public class JDBCSpring {
+
+    public static void main(String[] args) {
+
+    }
+}
