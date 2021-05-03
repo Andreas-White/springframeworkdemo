81318b4282f59ae3c731d298d4456555620d6694
Andreas Manos <antmanos42@gmail.com>
2021-05-03 02:22:35 +0200

    using JDBCTemplate class

--- src/main/java/com/example/jbrainsDataDemo/jdbcSpring/JDBCSpring.java
+++ src/main/java/com/example/jbrainsDataDemo/jdbcSpring/JDBCSpring.java
@@ -13,5 +13,6 @@ public class JDBCSpring {
         JDBCDaoSpring dao = context.getBean("JDBCDaoSpring", JDBCDaoSpring.class);
         Circle circle = dao.getCircle(1);
         System.out.println(circle.getName());
+        System.out.println(dao.getCircleCount());
     }
 }
--- src/main/java/com/example/jbrainsDataDemo/jdbcSpring/dao/JDBCDaoSpring.java
+++ src/main/java/com/example/jbrainsDataDemo/jdbcSpring/dao/JDBCDaoSpring.java
@@ -2,6 +2,7 @@ package com.example.jbrainsDataDemo.jdbcSpring.dao;
 
 import com.example.jbrainsDataDemo.jdbcSpring.model.Circle;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
@@ -10,13 +11,19 @@ import java.sql.*;
 @Component
 public class JDBCDaoSpring {
 
-    @Autowired
+
     private DataSource dataSource;
+    private JdbcTemplate jdbcTemplate = new JdbcTemplate();
+
+    public JdbcTemplate getJdbcTemplate() { return jdbcTemplate; }
+
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) { this.jdbcTemplate = jdbcTemplate; }
 
     public DataSource getDataSource() {
         return dataSource;
     }
 
+    @Autowired
     public void setDataSource(DataSource dataSource) {
         this.dataSource = dataSource;
     }
@@ -27,19 +34,19 @@ public class JDBCDaoSpring {
 
         try {
 
-
             conn = dataSource.getConnection();
-            PreparedStatement statement = conn.prepareStatement("SELECT * FROM circle WHERE id=?");
-            statement.setInt(1,circleId);
+
+            PreparedStatement preparedStatement = conn.prepareStatement("SELECT * FROM circle WHERE id=?");
+            preparedStatement.setInt(1,circleId);
 
             Circle circle = null;
-            ResultSet result = statement.executeQuery();
+            ResultSet result = preparedStatement.executeQuery();
 
             if (result.next()) {
                 circle = new Circle(circleId, result.getString("name"));
             }
 
-            statement.close();
+            preparedStatement.close();
             result.close();
 
             return circle;
@@ -48,10 +55,16 @@ public class JDBCDaoSpring {
         }finally {
             try {
                 conn.close();
-            } catch (SQLException throwables) {
-                throwables.printStackTrace();
+            } catch (SQLException throwable) {
+                throwable.printStackTrace();
             }
         }
         return null;
+    }
+
+    public int getCircleCount() {
+        String sql = "SELECT COUNT(*) FROM CIRCLE";
+        jdbcTemplate.setDataSource(getDataSource());
+        return jdbcTemplate.queryForObject(sql,Integer.class);
     }
 }
