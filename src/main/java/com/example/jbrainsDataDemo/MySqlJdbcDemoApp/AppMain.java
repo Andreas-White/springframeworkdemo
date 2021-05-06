7e7064d429401cf6533b199f9bd9b5e20f81f27f
Andreas Manos <antmanos42@gmail.com>
2021-05-06 02:05:09 +0200

    initialising demo app

---
+++ src/main/java/com/example/jbrainsDataDemo/MySqlJdbcDemoApp/AppMain.java
@@ -0,0 +1,4 @@
+package com.example.jbrainsDataDemo.MySqlJdbcDemoApp;
+
+public class AppMain {
+}
---
+++ src/main/java/com/example/jbrainsDataDemo/MySqlJdbcDemoApp/dao/UserDao.java
@@ -0,0 +1,4 @@
+package com.example.jbrainsDataDemo.MySqlJdbcDemoApp.dao;
+
+public class UserDao {
+}
